77c6eb83795e17e6e51a99c7eab17067295249b5
shakeer <shakick@gmail.com>
2022-04-14 00:23:58 +0530

    change for deploy

--- src/main/java/com/shakeer/company/CompanyApplication.java
+++ src/main/java/com/shakeer/company/CompanyApplication.java
@@ -2,12 +2,16 @@ package com.shakeer.company;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class CompanyApplication {
 
-	public static void main(String[] args) {SpringApplication.run(CompanyApplication.class, args);
-
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyApplication.class, args);
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String hashedPassword = passwordEncoder.encode("passwordadmin");
+		System.out.println(" password  :::" + hashedPassword);
 	}
-
 }
+
